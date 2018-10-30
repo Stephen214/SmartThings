62079384655b8ab6e9e1e36fd68aa34f7d6da33d
Stephen Sprunk <stephen@sprunk.org>
2018-10-30 16:26:16 -0500

    update to current versions

---
+++ devicetypes/stephen214/mbt.src/mbt.groovy
@@ -0,0 +1,41 @@
+/**
+ *  Mbt
+ *
+ *  Copyright 2018 Stephen Sprunk
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Mbt", namespace: "Stephen214", author: "Stephen Sprunk") {
+		capability "Momentary"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+
+}
+
+// handle commands
+def push() {
+	log.debug "Executing 'push'"
+	// TODO: handle 'push' command
+}
